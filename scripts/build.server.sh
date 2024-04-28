339ee2405d4e3e4551495f96fff43ee776274dea
cnphpbb <moqiruyi@gmail.com>
2024-04-28 16:59:35 +0800

    chore(projects): server build shell

--- scripts/build.server.sh
+++ scripts/build.server.sh
@@ -1,50 +1,76 @@
+#!/usr/bin/env sh
+
 export GO111MODULE=auto
 export COMMIT=`git rev-parse HEAD`
 
 
 
 macArm64() {
-    export GOOS=darwin
-    export GOARCH=arm64
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_darwin_arm64 Spark/server
+  export GOOS=darwin
+  export GOARCH=arm64
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_darwin_arm64 Spark/server
 }
 macAmd64() {
-    export GOOS=darwin
-    export GOARCH=amd64
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_darwin_amd64 Spark/server
+  export GOOS=darwin
+  export GOARCH=amd64
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_darwin_amd64 Spark/server
 }
 
 buildMacOS() {
-    if [ $1=="arm64" ]; then
-        macArm64
-    fi
-    if [ $1=="amd64" ]; then
-        macAmd64
-    fi
+  macArm64
+  macAmd64
 }
 
+linuxArm() {
+  export GOOS=linux
+  export GOARCH=arm
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_arm Spark/server
+}
+linux386() {
+  export GOOS=linux
+  export GOARCH=386
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_i386 Spark/server
+}
+linuxArm64() {
+  export GOOS=linux
+  export GOARCH=arm64
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_arm64 Spark/server
+}
+linuxAmd64() {
+  export GOOS=linux
+  export GOARCH=amd64
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_amd64 Spark/server
+}
 
 buildLinux() {
-    export GOOS=linux
-    export GOARCH=arm
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_arm Spark/server
-    export GOARCH=386
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_i386 Spark/server
-    export GOARCH=arm64
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_arm64 Spark/server
-    export GOARCH=amd64
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_linux_amd64 Spark/server
+  linuxArm
+  linux386
+  linuxArm64
+  linuxAmd64
+}
+
+windows386() {
+  export GOOS=windows
+  export GOARCH=386
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_windows_i386.exe Spark/server
+}
+windowsArm64() {
+  export GOOS=windows
+  export GOARCH=arm64
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_windows_arm64.exe Spark/server
+}
+windowsAmd64() {
+  export GOOS=windows
+  export GOARCH=amd64
+  go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_windows_amd64.exe Spark/server
 }
 
 buildWindows() {
-    export GOOS=windows
-    export GOARCH=386
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_windows_i386.exe Spark/server
-    export GOARCH=arm64
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_windows_arm64.exe Spark/server
-    export GOARCH=amd64
-    go build -ldflags "-s -w -X 'Spark/server/config.COMMIT=$COMMIT'" -tags=jsoniter -o ./releases/server_windows_amd64.exe Spark/server
+    windows386
+    windowsArm64
+    windowsAmd64
 }
+
 AllBuild() {
     buildMacOS
     buildLinux
